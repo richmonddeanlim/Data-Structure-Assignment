66fca220b55c3e6cfe86bcd9edd20f8eaa8579fe
gildedkiwi <evan.ferdinand2012@gmail.com>
2026-04-25 16:54:18 +0800

    Adding sorting on array and the amount of time executed

---
+++ array_program/Sorting.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include "ResidentArray.hpp"
+
+namespace Sorting {
+    double sortByAge(ResidentArray& arr);
+    double sortByDistance(ResidentArray& arr);
+    double sortByEmission(ResidentArray& arr);
+    double sortByTransport(ResidentArray& arr); 
+}
