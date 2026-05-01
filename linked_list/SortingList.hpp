1445a6cac0a719b0b4292d76b4ba652cf766b3eb
EvanFerdinand <evan.ferdinand2012@gmail.com>
2026-05-01 23:22:55 +0800

    add sortByEmission

--- linked_list/SortingList.hpp
+++ linked_list/SortingList.hpp
@@ -1,10 +1,11 @@
 #pragma once
 #include "LinkedList.hpp"
+#include "Residents.hpp"
 
 namespace SortingList {
     // Sorting functions for Linked List
-    // These return the time taken in seconds (matching array experiment)
-    double sortByAge(LinkedList& list);
-    double sortByDistance(LinkedList& list);
-    double sortByTransport(LinkedList& list);
+    double sortByAge(LinkedList& list, size_t& memUsed);
+    double sortByDistance(LinkedList& list, size_t& memUsed);
+    double sortByEmission(LinkedList& list, size_t& memUsed);
+    double sortByTransport(LinkedList& list, size_t& memUsed);
 }
