005829cf3201d5f366724e003f710d5e9152753a
EvanFerdinand <evan.ferdinand2012@gmail.com>
2026-05-03 17:11:35 +0800

    fixed precise timing

--- linked_list/SortingList.cpp
+++ linked_list/SortingList.cpp
@@ -2,135 +2,89 @@
 #include "LinkedList.hpp"
 #include "Residents.hpp"
 #include <chrono>
+#include <iomanip>
+#include <iostream>
 using namespace std;
 using namespace std::chrono;
 
 namespace SortingList {
     // Sort By Age
-    double sortByAge(LinkedList& list, size_t& memUsed) {
+    double sortByAge(LinkedList& list, size_t& memUsed, bool isAllCities) {
         auto start = high_resolution_clock::now();
-        
         for (Node* i = list.getHead(); i != nullptr && i->next != nullptr; i = i->next) {
             Node* minNode = i;
             for (Node* j = i->next; j != nullptr; j = j->next) {
-                if (j->data.age < minNode->data.age) {
-                    minNode = j;
-                }
+                if (j->data.age < minNode->data.age) minNode = j;
             }
-            if (minNode != i) {
-                swap(i->data, minNode->data);
-            }
+            if (minNode != i) swap(i->data, minNode->data);
         }
-
         auto end = high_resolution_clock::now();
-        
         duration<double> elapsed = end - start;
-
-        // memory usage
         memUsed = list.totalMemoryBytes();
-
-        // print
+        duration<double, milli> elapsed_ms = elapsed;
         cout << "\n[Performance]\n";
-        auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(elapsed);
-        cout << "Execution Time: " << elapsed_ms.count() << " milliseconds\n";
+        cout << "Execution Time: " << fixed << setprecision(4) << elapsed_ms.count() << " milliseconds\n";
         cout << "Memory Used   : " << memUsed << " bytes\n";
-
         return elapsed.count();
     }
 
     // Sort By Daily Distance
-    double sortByDistance(LinkedList& list, size_t& memUsed) {
+    double sortByDistance(LinkedList& list, size_t& memUsed, bool isAllCities) {
         auto start = high_resolution_clock::now();
-        
         for (Node* i = list.getHead(); i != nullptr && i->next != nullptr; i = i->next) {
             Node* minNode = i;
             for (Node* j = i->next; j != nullptr; j = j->next) {
-                if (j->data.dailyDistance < minNode->data.dailyDistance) {
-                    minNode = j;
-                }
+                if (j->data.dailyDistance < minNode->data.dailyDistance) minNode = j;
             }
-            if (minNode != i) {
-                swap(i->data, minNode->data);
-            }
+            if (minNode != i) swap(i->data, minNode->data);
         }
-
         auto end = high_resolution_clock::now();
-        
         duration<double> elapsed = end - start;
-
-        // memory usage
         memUsed = list.totalMemoryBytes();
-
-        // print
+        duration<double, milli> elapsed_ms = elapsed;
         cout << "\n[Performance]\n";
-        auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(elapsed);
-        cout << "Execution Time: " << elapsed_ms.count() << " milliseconds\n";
+        cout << "Execution Time: " << fixed << setprecision(4) << elapsed_ms.count() << " milliseconds\n";
         cout << "Memory Used   : " << memUsed << " bytes\n";
-
         return elapsed.count();
     }
 
     // Sort By Carbon Emission
-    double sortByEmission(LinkedList& list, size_t& memUsed) {
+    double sortByEmission(LinkedList& list, size_t& memUsed, bool isAllCities) {
         auto start = high_resolution_clock::now();
-        
         for (Node* i = list.getHead(); i != nullptr && i->next != nullptr; i = i->next) {
             Node* minNode = i;
             for (Node* j = i->next; j != nullptr; j = j->next) {
-                if (j->data.emission() < minNode->data.emission()) {
-                    minNode = j;
-                }
+                if (j->data.emission() < minNode->data.emission()) minNode = j;
             }
-            if (minNode != i) {
-                swap(i->data, minNode->data);
-            }
+            if (minNode != i) swap(i->data, minNode->data);
         }
-
         auto end = high_resolution_clock::now();
-        
         duration<double> elapsed = end - start;
-
-        // memory usage
         memUsed = list.totalMemoryBytes();
-
-        // print
+        duration<double, milli> elapsed_ms = elapsed;
         cout << "\n[Performance]\n";
-        auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(elapsed);
-        cout << "Execution Time: " << elapsed_ms.count() << " milliseconds\n";
+        cout << "Execution Time: " << fixed << setprecision(4) << elapsed_ms.count() << " milliseconds\n";
         cout << "Memory Used   : " << memUsed << " bytes\n";
-
         return elapsed.count();
     }
 
     // Sort By Transportation Mode
-    double sortByTransport(LinkedList& list, size_t& memUsed) {
+    double sortByTransport(LinkedList& list, size_t& memUsed, bool isAllCities) {
         auto start = high_resolution_clock::now();
-        
         for (Node* i = list.getHead(); i != nullptr && i->next != nullptr; i = i->next) {
             Node* minNode = i;
             for (Node* j = i->next; j != nullptr; j = j->next) {
-                if (j->data.modeOfTransport < minNode->data.modeOfTransport) {
-                    minNode = j;
-                }
+                if (j->data.modeOfTransport < minNode->data.modeOfTransport) minNode = j;
             }
-            if (minNode != i) {
-                swap(i->data, minNode->data);
-            }
+            if (minNode != i) swap(i->data, minNode->data);
         }
-
         auto end = high_resolution_clock::now();
-        
         duration<double> elapsed = end - start;
-
-        // memory usage
         memUsed = list.totalMemoryBytes();
-
-        // print
+        duration<double, milli> elapsed_ms = elapsed;
         cout << "\n[Performance]\n";
-        auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(elapsed);
-        cout << "Execution Time: " << elapsed_ms.count() << " milliseconds\n";
+        cout << "Execution Time: " << fixed << setprecision(4) << elapsed_ms.count() << " milliseconds\n";
         cout << "Memory Used   : " << memUsed << " bytes\n";
-
         return elapsed.count();
     }
 }
